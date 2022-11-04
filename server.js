51d1fb3ba4dc851558be093e74f6579f39836fd6
Raynara-Lima <raynaralima17@gmail.com>
2022-11-04 01:13:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var db = require("./db");
 
 app.get('/DefinirTempoForno', (req, res) => {
  // json = {"message": "success", "people": [{"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}, {"name": "Sergey Prokopyev", "craft": "ISS"}, {"name": "Dmitry Petelin", "craft": "ISS"}, {"name": "Frank Rubio", "craft": "ISS"}, {"name": "Nicole Mann", "craft": "ISS"}, {"name": "Josh Cassada", "craft": "ISS"}, {"name": "Koichi Wakata", "craft": "ISS"}, {"name": "Anna Kikina", "craft": "ISS"}], "number": 10}
-let json = JSON.parse(req.params)
+let json = JSON.parse(req.query[0])
   const data = JSON.stringify( json);
   res.send(data)
 })
