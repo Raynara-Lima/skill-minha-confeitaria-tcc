aee1a0fdf3441d23eddcea691c7a7ad65c055db9
Raynara-Lima <raynaralima17@gmail.com>
2022-11-03 23:13:23 -0300

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var db = require("./db");
 
 app.get('/DefinirTempoForno' , (req, res) => {
   json = {"message": "success", "people": [{"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}, {"name": "Sergey Prokopyev", "craft": "ISS"}, {"name": "Dmitry Petelin", "craft": "ISS"}, {"name": "Frank Rubio", "craft": "ISS"}, {"name": "Nicole Mann", "craft": "ISS"}, {"name": "Josh Cassada", "craft": "ISS"}, {"name": "Koichi Wakata", "craft": "ISS"}, {"name": "Anna Kikina", "craft": "ISS"}], "number": 10}
-   console.log(req)
+   console.log("req:"+req)
   res.send(json)  
   // const topicName = 'Car_input';
 
