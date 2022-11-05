bcb1efd5f84070fb0c6403b865af0964881aeda9
Raynara-Lima <raynaralima17@gmail.com>
2022-11-05 01:16:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var db = require("./db");
 
 app.post('/DefinirTempoForno', (req, res) => {
   let json = JSON.parse(req.query[0])
-  const data = JSON.stringify(json);
+  const data = {"nomePro": "bolo de cenoura", "hora": "10", "tempo": "10"};
   var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
   var info = new ProdutoNoForno(data);
     info.save(function (err) {
