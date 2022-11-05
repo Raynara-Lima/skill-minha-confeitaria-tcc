e02045168d38f6c9c8d4f38f9125ddd779404c41
Raynara-Lima <raynaralima17@gmail.com>
2022-11-05 01:09:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ var db = require("./db");
 app.post('/DefinirTempoForno', (req, res) => {
   let json = JSON.parse(req.query[0])
   const data = JSON.stringify(json);
-   
-  var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
-  var info = new ProdutoNoForno(data);
-    info.save(function (err) {
-      if (err) {
-          console.log("Error! " + err.message);
-          res.send(err)
-        }
-      else {
-        res.send(data)      
-        }
-      });
+   res.send(data)
+//   var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
+//   var info = new ProdutoNoForno(data);
+//     info.save(function (err) {
+//       if (err) {
+//           console.log("Error! " + err.message);
+//           res.send(err)
+//         }
+//       else {
+//         res.send(data)      
+//         }
+//       });
   
 })
 app.get('/', (req, res) => {
