a28cf4752649ce329dbe2d4c0b3265f41b58344f
Raynara-Lima <raynaralima17@gmail.com>
2022-11-06 19:53:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const port = process.env.PORT || 8080
 var db = require("./db");
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.post('/DefinirTempoForno', (req, res) => {
   let json = JSON.parse(req.query[0])
@@ -20,6 +23,7 @@ app.post('/DefinirTempoForno', (req, res) => {
       });
   
 })
+
 app.get('/ConsultarTempoRestante', (req, res) => {
   let json = JSON.parse(req.query[0]);
   var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
@@ -32,41 +36,22 @@ app.get('/ConsultarTempoRestante', (req, res) => {
       }
     })  
 })
+
+app.post('/AdicionarIngredienteEstoque', (req, res) => {
+  let json = JSON.parse(req.query[0])
+  data = {"ingrediente": json.ingrediente, "quantidade": json.quantidade};
+  var Estoque = db.Mongoose.model('estoque', db.estoqueSchema, 'estoque');
+  Estoque.findOneAndUpdate({ingrediente: json.ingrediente}, data, {upsert: true}, function(err, doc) {
+    if (err) return res.send(500, {error: err});
+    return res.send({code: 1});
+});
+})
+
 app.get('/', (req, res) => {
   json = {"message": "success", "people": [{"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}, {"name": "Sergey Prokopyev", "craft": "ISS"}, {"name": "Dmitry Petelin", "craft": "ISS"}, {"name": "Frank Rubio", "craft": "ISS"}, {"name": "Nicole Mann", "craft": "ISS"}, {"name": "Josh Cassada", "craft": "ISS"}, {"name": "Koichi Wakata", "craft": "ISS"}, {"name": "Anna Kikina", "craft": "ISS"}], "number": 10}
 res.send(json)
 })
-// app.get('/', (req, res) => {
-//     // json = {"message": "success", "people": [{"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}, {"name": "Sergey Prokopyev", "craft": "ISS"}, {"name": "Dmitry Petelin", "craft": "ISS"}, {"name": "Frank Rubio", "craft": "ISS"}, {"name": "Nicole Mann", "craft": "ISS"}, {"name": "Josh Cassada", "craft": "ISS"}, {"name": "Koichi Wakata", "craft": "ISS"}, {"name": "Anna Kikina", "craft": "ISS"}], "number": 10}
-//     json = {"nomePro": "bolo de cenoura", "hora": "10", "tempo": "t"};
-//     var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
-//     var info = new ProdutoNoForno(json);
-//     info.save(function (err) {
-//       if (err) {
-//           console.log("Error! " + err.message);
-//           return err;
-//         }
-//       else {
-//           console.log("Post saved");
-        
-//         }
-//       });
-//     var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
-//     ProdutoNoForno.find({}).lean().exec(
-//       function (e, docs) {
-//     console.log(docs)
-//       })
-//     // res.send(docs)   
-// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
-function getInfos(){
-  var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
-  const docs =  ProdutoNoForno.find({}).lean().exec();
-  console.log(docs)
-  res.send(docs)
-}
+
 
