2fcaa5936b343e6a691a475e050a1f23abf86d1b
Raynara-Lima <raynaralima17@gmail.com>
2022-11-05 21:55:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/DefinirTempoForno', (req, res) => {
 app.get('/ConsultarTempoRestante', (req, res) => {
   let json = JSON.parse(req)
   const data = JSON.stringify( json);
-  res.send(data)
+  res.send(json)
 
 //   data = {"nomePro": json.nomePro, "hora": json.hora, "tempo": json.tempo};//JSON.stringify(json);
 //   var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
