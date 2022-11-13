637fc9d459fda29d6d2b9cc3c880aba7bf120529
Raynara-Lima <raynaralima17@gmail.com>
2022-11-13 02:42:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -110,28 +110,28 @@ app.get('/forno', (req, res) => {
 
 
 app.get('/', async (req, res) => {
-//   let count = 0, forno;
-//   var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
-//   var Forno = db.Mongoose.model('forno', db.fornoSchema, 'forno');
-//    const docs = await ProdutoNoForno.find().exec();
+  let count = 0, forno;
+  var ProdutoNoForno = db.Mongoose.model('produtoNoForno', db.produtoNoFornoSchema, 'produtoNoForno');
+  var Forno = db.Mongoose.model('forno', db.fornoSchema, 'forno');
+   const docs = await ProdutoNoForno.find().exec();
 
-//     docs.forEach(
-//       async function (element) {
-//       let tempoRestante = calcularTempoRestante(element.hora, element.tempo)
-//       console.log("tempo restante: ", tempoRestante)
-//       if(tempoRestante == 0){
-//         await Forno.findOneAndUpdate({"id": 0}, {"notificacao": 1} ,{upsert: true}).exec()
-//         await ProdutoNoForno.findOneAndDelete({"nomePro": element.nomePro}).exec()
-//       }
-//     })
-//     count = await ProdutoNoForno.countDocuments({}).exec()
+    docs.forEach(
+      async function (element) {
+      let tempoRestante = calcularTempoRestante(element.hora, element.tempo)
+      console.log("tempo restante: ", tempoRestante)
+      if(tempoRestante == 0){
+        await Forno.findOneAndUpdate({"id": 0}, {"notificacao": 1} ,{upsert: true}).exec()
+        await ProdutoNoForno.findOneAndDelete({"nomePro": element.nomePro}).exec()
+      }
+    })
+    count = await ProdutoNoForno.countDocuments({}).exec()
   
-//     console.log("Count: ", count)
+    console.log("Count: ", count)
    
-//     if(count === 0){
-//       Forno.findOneAndUpdate({"id": 0}, {"isLigado": 0} ,{upsert: true}).exec();  
-//     }
-//     forno = await Forno.findOne().lean().exec();
+    if(count === 0){
+      Forno.findOneAndUpdate({"id": 0}, {"isLigado": 0} ,{upsert: true}).exec();  
+    }
+    forno = await Forno.findOne().lean().exec();
 //     res.send({"isLigado": 1, "notificacao": 0})
 
 
