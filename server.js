07747d52d94676a4883ff78d70ab008ffaa6e4af
Raynara-Lima <raynaralima17@gmail.com>
2022-11-13 03:26:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -118,10 +118,10 @@ app.get('/', async (req, res) => {
     docs.forEach(
       async function (element) {
       let tempoRestante = calcularTempoRestante(element.hora, element.tempo)
-      if(tempoRestante == 0){
-        await Forno.findOneAndUpdate({"id": 0}, {"notificacao": 1} ,{upsert: true}).exec()
-        await ProdutoNoForno.findOneAndDelete({"nomePro": element.nomePro}).exec()
-      }
+//       if(tempoRestante == 0){
+//         await Forno.findOneAndUpdate({"id": 0}, {"notificacao": 1} ,{upsert: true}).exec()
+//         await ProdutoNoForno.findOneAndDelete({"nomePro": element.nomePro}).exec()
+//       }
     })
     count = await ProdutoNoForno.countDocuments({}).exec()
   
